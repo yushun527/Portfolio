10f92327d2fcd95957ade57cfde4a1819842e7b3
yushun527 <yushun527gambare@gmail.com>
2022-03-02 22:22:08 +0900

    プロフィール追加

--- src/Components/Aboutme.tsx
+++ src/Components/Aboutme.tsx
@@ -1,17 +1,23 @@
-import { Avatar } from '@mui/material';
+import { Avatar, Container } from '@mui/material';
 import React from 'react';
 
 const Aboutme: React.FC = () => {
   return (
     <>
       <h1 className="plofile">Plofile</h1>
-      <h2>Name</h2>
-      <Avatar
-        alt=""
-        src="https://lh3.googleusercontent.com/pw/AM-JKLW9N4Oxxh0Z8ZDC8K9p7J2UzGjRJ5OHvmqJkJxTMLTrOA4HR8_ubVAfhCiLbatSzPnBzHownSZ0AH4gWtN_C7XHxsD7NcH4yPaA9SbZyDf3U71vnlhvO2jQBRNLSs4gvVRnUp8YURDRXEqNJkTC9yDw=s1013-no?authuser=0"
-        sx={{ width: 200, height: 200 }}
-        className="picture2"
-      />
+      <Container className="box">
+        <>
+          <h2>Name: Yushun Ando</h2>
+          <h2>Birthday : May/27th/2000</h2>
+          <h2>School: Nanzan University</h2>
+        </>
+        <Avatar
+          alt=""
+          src="https://lh3.googleusercontent.com/pw/AM-JKLW9N4Oxxh0Z8ZDC8K9p7J2UzGjRJ5OHvmqJkJxTMLTrOA4HR8_ubVAfhCiLbatSzPnBzHownSZ0AH4gWtN_C7XHxsD7NcH4yPaA9SbZyDf3U71vnlhvO2jQBRNLSs4gvVRnUp8YURDRXEqNJkTC9yDw=s1013-no?authuser=0"
+          sx={{ width: 200, height: 200 }}
+          className="picture2"
+        />
+      </Container>
     </>
   );
 };
