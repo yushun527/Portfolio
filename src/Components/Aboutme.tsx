9ada478d9b1868edc05833455902f50dc66adbe0
yushun527 <yushun527gambare@gmail.com>
2022-03-03 03:05:20 +0900

    完成

--- src/Components/Aboutme.tsx
+++ src/Components/Aboutme.tsx
@@ -1,24 +1,33 @@
-import { Avatar, Container } from '@mui/material';
+import { Avatar } from '@mui/material';
 import React from 'react';
 
 const Aboutme: React.FC = () => {
   return (
-    <>
+    <div className="background">
       <h1 className="plofile">Plofile</h1>
-      <Container className="box">
-        <Avatar
-          alt=""
-          src="https://lh3.googleusercontent.com/pw/AM-JKLW9N4Oxxh0Z8ZDC8K9p7J2UzGjRJ5OHvmqJkJxTMLTrOA4HR8_ubVAfhCiLbatSzPnBzHownSZ0AH4gWtN_C7XHxsD7NcH4yPaA9SbZyDf3U71vnlhvO2jQBRNLSs4gvVRnUp8YURDRXEqNJkTC9yDw=s1013-no?authuser=0"
-          sx={{ width: 200, height: 200 }}
-          className="picture2"
-        />
-        <>
-          <h2>Name: Yushun Ando</h2>
-          <h2>Birthday: 2000年5月27日</h2>
-          <p>大学三年生の時にプログラミングを始めました。</p>
-        </>
-      </Container>
-    </>
+      <Avatar
+        alt=""
+        src="https://lh3.googleusercontent.com/pw/AM-JKLW9N4Oxxh0Z8ZDC8K9p7J2UzGjRJ5OHvmqJkJxTMLTrOA4HR8_ubVAfhCiLbatSzPnBzHownSZ0AH4gWtN_C7XHxsD7NcH4yPaA9SbZyDf3U71vnlhvO2jQBRNLSs4gvVRnUp8YURDRXEqNJkTC9yDw=s1013-no?authuser=0"
+        sx={{ width: 200, height: 200 }}
+        className="picture2"
+      />
+      <div>
+        <h2 className="plofile">Name: Yushun Ando</h2>
+        <h2 className="plofile">Birthday: 2000年5月27日</h2>
+        <p className="aboutme">
+          現在南山大学に通う学生です。出身は北海道です。高校まで北海道にいました。
+          <br />
+          大学では政策系や環境問題などについて学んでいます。
+          <br />
+          プログラミングは大学三年生の時に始めました。
+          <br />
+          始めた理由は、エンジニアの需要が高まっているということと、私自身ものづくりが好きだからです。
+          <br />
+          趣味や好きなことは、ギター、ゲーム、映画鑑賞、漫画、音楽です。
+          <br />
+        </p>
+      </div>
+    </div>
   );
 };
 
--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Contact: React.FC = () => {
-  return <h1>Contact</h1>;
+  return (
+    <div className="background">
+      <h1 className="form"> Contact</h1>
+      <Form className="textarea">
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>coment</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+      <Button variant="primary" size="lg" active className="button">
+        送信
+      </Button>
+    </div>
+  );
 };
 
 export default Contact;
